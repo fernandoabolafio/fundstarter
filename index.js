96ed423be295c526a7eed4c59da7ac6245d7d077
Ubuntu <ubuntu@ip-172-31-38-70.us-west-2.compute.internal>
2016-03-14 22:30:18 +0000

    index.js changes to run the server with pure nodejs code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var express = require('express')
-var app = express()
+var http = require('http');
 
-/* serves all the static files*/
-app.set('port', (process.env.PORT || 8080))
-app.use(express.static(__dirname + '/public'))
 
-/*serves main page*/
-app.get('/', function(request, response) {
-response.sendfile('index.html') 
-})
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var requestListener = function (req, res) {
+  res.writeHead(200);
+
+
+  res.end("Hello word");
+
+
+}
+
+var server = http.createServer(requestListener);
+server.listen(8080);
